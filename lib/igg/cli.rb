b42f56efeb2f0419e174d875eb0190965f88a42f
eiffel qiu <eiffelqiu@qq.com>
2015-01-19 15:31:27 +0800

    fix bug

--- lib/igg/cli.rb
+++ lib/igg/cli.rb
@@ -113,12 +113,17 @@ class Igg::CLI < Thor
 
   def self.source_root; File.dirname(__FILE__) end
 
-  class_option :width, :type => :numeric, :default => 320, :required => false, :aliases => "-w", :desc => "width"
-  class_option :height, :type => :numeric, :default => 240, :required => false, :aliases => "-h", :desc => "height"
-
   %w[project entity level].each do |type|
+    if type == 'entity' 
+        method_option :width, :type => :numeric, :default => 16, :required => false, :aliases => "-w", :desc => "width"
+        method_option :height, :type => :numeric, :default => 16, :required => false, :aliases => "-h", :desc => "height"
+    else
+        method_option :width, :type => :numeric, :default => 320, :required => false, :aliases => "-w", :desc => "width"
+        method_option :height, :type => :numeric, :default => 240, :required => false, :aliases => "-h", :desc => "height"
+    end      
+
     class_eval %{
-      desc "#{type} [NAME]", "generate an ImpactJS Game #{type}"
+      desc "#{type} [name]", "generate an ImpactJS Game #{type}"
       def #{type}(name=nil) end
     }
   end
@@ -129,8 +134,27 @@ class Igg::CLI < Thor
     @name, @width, @height  = args[0][0] || "#{method}", options[:width], options[:height]
     @name = "#{@name}".downcase                                  
     if method == 'server' then
-      Server.run!
-    elsif method != 'help' 
+      if File.exist?('lib/weltmeister/config.js')
+        File.write("lib/weltmeister/config.js",File.open("lib/weltmeister/config.js",&:read).gsub(".php",""))
+        puts 
+        puts "*" * 80
+        puts "modified lib/weltmeister/config.js file, remove all '.php' suffix in api call "
+        puts "*" * 80
+        puts         
+        Server.run!
+      else
+        puts "*" * 60
+        puts "Can't run without impact and weltmeister, you must do as follow"
+        puts 
+        puts "1: Copy 'impact' folder to current 'lib' subdirectory.  "
+        puts "2: Copy 'weltmeister' folder to current 'lib' subdirectory. "
+        puts "3: Copy 'weltmeister.html' folder to current directory. "        
+        puts "*" * 60
+        exit
+      end      
+    elsif method == 'help' 
+
+    else
       send "build_#{method}" 
     end
 
