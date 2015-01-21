dbac8a93cdd2872f9de75c42e0240adaf02c6271
eiffel qiu <eiffelqiu@qq.com>
2015-01-21 12:46:21 +0800

    add toggle debug button

--- lib/igg/builder/project_builder.rb
+++ lib/igg/builder/project_builder.rb
@@ -8,6 +8,7 @@ module Igg
       	empty_directory "#{@name}/lib/game/levels"
       	empty_directory "#{@name}/media"
       	empty_directory "#{@name}/media/sounds"
+        empty_directory "#{@name}/js"
 
       	copy_file 'builder/templates/project/sounds/death.mp3',"#{@name}/media/sounds/death.mp3"
       	copy_file 'builder/templates/project/sounds/death.ogg',"#{@name}/media/sounds/death.ogg"
@@ -17,6 +18,9 @@ module Igg
       	copy_file 'builder/templates/project/sounds/shoot.ogg',"#{@name}/media/sounds/shoot.ogg"
       	copy_file 'builder/templates/project/sounds/theme.mp3',"#{@name}/media/sounds/theme.mp3"
       	copy_file 'builder/templates/project/sounds/theme.ogg',"#{@name}/media/sounds/theme.ogg"
+
+        copy_file 'builder/templates/project/jquery.min.js',"#{@name}/js/jquery.min.js"
+        copy_file 'builder/templates/project/toggleDebug.js',"#{@name}/js/toggleDebug.js"
 
       	copy_file 'builder/templates/project/04b03.font.png',"#{@name}/media/04b03.font.png"
         template 'builder/templates/project/main.tt', "#{@name}/lib/game/main.js"
